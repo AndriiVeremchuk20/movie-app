288b1ea86eabbb67111bde39c61acc4c203b2210
MonkeyAndrei <andrejveremcuk712@gmail.com>
2023-03-29 00:09:31 +0300

    added credit card form

---
+++ client/src/components/paymentForm.tsx
@@ -0,0 +1,136 @@
+import React from "react";
+import premium from "@/api/premium";
+import { useMutation } from "@tanstack/react-query";
+import { useRouter } from "next/router";
+import { useForm } from "react-hook-form";
+
+const PaymentForm: React.FC = () => {
+  const router = useRouter();
+
+  const buyPremium = useMutation(premium.buyPremium, {
+    onSuccess() {
+      router.reload();
+    },
+    onError() {},
+  });
+
+  const onBuyPremiumClick = () => {
+    buyPremium.mutate();
+  };
+
+  return (
+    <div className="h-auto w-[550px] rounded-3xl border-[4px] border-inherit border-black bg-[url('/img/bg-credit-card.jpg')] bg-cover">
+      <form className="flex w-full flex-wrap gap-2 p-3">
+        <label className="relative flex w-full flex-col">
+          <span className="mb-3 text-xl font-bold text-white">Card number</span>
+          <input
+            className="peer rounded-md border-2 border-gray-200 py-2 pl-12 pr-2 placeholder-gray-300"
+            type="text"
+            placeholder="0000 0000 0000 0000"
+            maxLength={16}
+          />
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            className="absolute bottom-0 left-0 -mb-0.5 h-6 w-6 translate-x-1/2 -translate-y-1/2 transform text-black peer-placeholder-shown:text-gray-300"
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke="currentColor"
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              strokeWidth="2"
+              d="M3 10h18M7 15h1m4 0h1m-7 4h12a3 3 0 003-3V8a3 3 0 00-3-3H6a3 3 0 00-3 3v8a3 3 0 003 3z"
+            />
+          </svg>
+        </label>
+
+        <div className=" mt-[100px] mb-8 flex w-full justify-around pr-5">
+          <label className="relative flex w-36 flex-col">
+            <span className="mb-3 text-xl font-bold text-white">
+              Expire date
+            </span>
+            <input
+              className="peer rounded-md border-2 border-gray-200 py-2 pl-12 pr-2 placeholder-gray-300"
+              type="text"
+              placeholder="MM/YY"
+              maxLength={4}
+            />
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              className="absolute bottom-0 left-0 -mb-0.5 h-6 w-6 translate-x-1/2 -translate-y-1/2 transform text-black peer-placeholder-shown:text-gray-300"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke="currentColor"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="2"
+                d="M8 7V3m8 4V3m-9 8h10M5 21h14a2 2 0 002-2V7a2 2 0 00-2-2H5a2 2 0 00-2 2v12a2 2 0 002 2z"
+              />
+            </svg>
+          </label>
+
+          <label className="relative flex w-24 flex-col">
+            <span className="mb-3 flex items-center gap-3 text-xl font-bold text-white">
+              CVC/CVV
+              <span className="group relative">
+                <span className="absolute -right-2 top-1/2 hidden w-max translate-x-full -translate-y-1/2 transform items-center justify-center bg-black px-2 py-1 text-xs text-white group-hover:flex">
+                  {" "}
+                  Hey ceci est une infobulle !
+                </span>
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  className="h-4 w-4"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  stroke="currentColor"
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    strokeWidth="2"
+                    d="M8.228 9c.549-1.165 2.03-2 3.772-2 2.21 0 4 1.343 4 3 0 1.4-1.278 2.575-3.006 2.907-.542.104-.994.54-.994 1.093m0 3h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z"
+                  />
+                </svg>
+              </span>
+            </span>
+            <input
+              className="peer rounded-md border-2 border-gray-200 py-2 pl-12 pr-2 placeholder-gray-300"
+              type="text"
+              placeholder="&bull;&bull;&bull;"
+              maxLength={3}
+            />
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              className="absolute bottom-0 left-0 -mb-0.5 h-6 w-6 translate-x-1/2 -translate-y-1/2 transform text-black peer-placeholder-shown:text-gray-300"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke="currentColor"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="2"
+                d="M12 15v2m-6 4h12a2 2 0 002-2v-6a2 2 0 00-2-2H6a2 2 0 00-2 2v6a2 2 0 002 2zm10-10V7a4 4 0 00-8 0v4h8z"
+              />
+            </svg>
+          </label>
+        </div>
+        <div className="h-10 w-full">
+        <button
+          type="submit"
+          disabled
+          onClick={onBuyPremiumClick}
+          className="h-full w-full rounded-b-2xl bg-orange-400 text-xl font-bold text-white hover:bg-orange-600 disabled:bg-gray-500"
+        >
+          Buy
+        </button>
+      </div>
+      </form>
+    </div>
+  );
+};
+
+export default PaymentForm;
--- client/src/pages/profile/premium/index.tsx
+++ client/src/pages/profile/premium/index.tsx
@@ -1,11 +1,13 @@
 import premium from "@/api/premium";
 import appRoutes from "@/appRoutes";
 import { appUserAtom } from "@/atom";
+import PaymentForm from "@/components/paymentForm";
 import Recommendations from "@/components/recommendations";
 import { BaseMovie } from "@/types/movie";
 import { isAuthed } from "@/utils/isAuthed";
 import { useMutation } from "@tanstack/react-query";
 import { useAtom } from "jotai";
+import Head from "next/head";
 import { useRouter } from "next/router";
 import React, { useEffect, useState } from "react";
 
@@ -21,17 +23,6 @@ export const Premium = () => {
     onError() {},
   });
 
-  const buyPremium = useMutation(premium.buyPremium, {
-    onSuccess() {
-      router.reload();
-    },
-    onError() {},
-  });
-
-  const onBuyPremiumClick = () => {
-    buyPremium.mutate();
-  };
-
   useEffect(() => {
     if (!isAuthed()) {
       router.replace(appRoutes.login);
@@ -44,30 +35,32 @@ export const Premium = () => {
 
   if (user)
     return (
-      <div className="flex max-h-fit min-h-screen justify-center bg-lime-100 dark:bg-sky-900">
-        <div className="mt-36 mb-10 flex h-auto w-screen flex-col bg-emerald-500 pb-10 dark:bg-gray-800 md:w-3/4">
-          <div className="flex h-fit w-full bg-yellow-600 py-2 text-6xl font-bold text-white">
-            Premium
-          </div>
-          <div className="mt-3 ml-4 text-2xl text-white">
-            {!user.isPremium
-              ? `Buy premium you get access to movies that are only available for premium user.`
-              : `You're alredy premium`}
-          </div>
-          <div className="m-3">
-            <Recommendations movies={movies} />
-          </div>
-          <div className="flex w-full flex-col">
-            <div className="m-auto mb-5 text-5xl text-white">18$</div>
-            <button
-              onClick={onBuyPremiumClick}
-              className="m-auto w-fit rounded-sm bg-yellow-600 px-3 py-2 text-3xl font-bold text-black hover:bg-yellow-700 hover:shadow-sm hover:shadow-yellow-700"
-            >
-              Buy Premium
-            </button>
+      <>
+        <Head>
+          <title>Premium</title>
+        </Head>
+        <div className="flex max-h-fit min-h-screen justify-center bg-lime-100 dark:bg-sky-900">
+          <div className="mt-36 mb-10 flex h-auto w-screen flex-col  bg-neutral-500 pb-10 dark:bg-neutral-800 bg-opacity-40 dark:bg-opacity-50 md:w-3/4">
+            <div className="flex h-fit w-full bg-yellow-600 py-2 text-6xl font-bold text-white">
+              Premium
+            </div>
+            <div className="mt-3 ml-4 text-2xl text-white">
+              {!user.isPremium
+                ? `Buy premium you get access to movies that are only available for premium user.`
+                : `You're alredy premium`}
+            </div>
+            <div className="m-3">
+              <Recommendations movies={movies} />
+            </div>
+            <div className="flex w-full flex-col">
+            <div className="m-auto my-5 text-5xl text-white">18$</div>
+              <div className="w-full flex justify-center">
+                <PaymentForm />
+              </div>           
+            </div>
           </div>
         </div>
-      </div>
+      </>
     );
 
   return null;
